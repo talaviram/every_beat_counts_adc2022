a5be9975c0768c1dc7478bf905e6b0d1f4751d8b
Tal Aviram <me@talaviram.com>
2022-10-28 09:11:18 +0300

    "kinda" synced quarter impulse
    Note: ppqSamplesCounter is no longer needed since the quarter state is now
    provided from the host's transport as we sync to it.

--- SimpleMetronome/Source/PluginProcessor.cpp
+++ SimpleMetronome/Source/PluginProcessor.cpp
@@ -85,7 +85,6 @@ void SimpleMetronomeAudioProcessor::changeProgramName (int, const String&)
 
 void SimpleMetronomeAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    ppqSamplesCounter = 0;
 }
 
 void SimpleMetronomeAudioProcessor::releaseResources()
@@ -130,18 +129,12 @@ void SimpleMetronomeAudioProcessor::processBlock (AudioBuffer<float>& buffer, Mi
     }
 
     const auto quarterInSamples = getSampleRate() / tempoUtils.getQuarterInSeconds();
-    ppqSamplesCounter += buffer.getNumSamples();
+    const auto nextQuarterInSamples = roundToInt ((1.0 - tempoUtils.getSubQuarterDivision()) * quarterInSamples);
+
     std::optional<int> tickAt;
 
-    if (ppqSamplesCounter > quarterInSamples)
-    {
-        ppqSamplesCounter %= roundToInt (quarterInSamples);
-        tickAt = buffer.getNumSamples() - ppqSamplesCounter;
-
-        // missed deadline. don't tick
-        if (*tickAt < 0)
-            tickAt = {};
-    }
+    if (nextQuarterInSamples < buffer.getNumSamples())
+        tickAt = nextQuarterInSamples;
 
     if (tickAt.has_value())
     {
